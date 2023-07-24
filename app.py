7dd53bede18e1ba5c3ee1968a7f1a2d719aede64
YukiF0511 <140325157+YukiF0511@users.noreply.github.com>
2023-07-24 09:30:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # 以下を「app.py」に書き込み
 import streamlit as st
 import openai
-import secret_keys  # 外部ファイルにAPI keyを保存
+#import secret_keys  # 外部ファイルにAPI keyを保存
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
